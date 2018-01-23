4d832e81fe8d5a2f8109343740d7d9d2660b734e
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2018-01-23 11:06:13 +0500

    Initial Commit

---
+++ CareemAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CareemAssignment
+//
+//  Created by Muhammad Tayyab Akram on 23/01/2018.
+//  Copyright © 2018 Muhammad Tayyab Akram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
